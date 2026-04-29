2dff5e4f2a21905d8b6d0781f46ef2fc6b8796c8
Igreja Videira Jataí <secretariavideirajataigo@gmail.com>
2026-04-29 10:22:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js - Configurações centralizadas do Sistema Videira
 // ATUALIZE ESTE ARQUIVO PARA ALTERAR CHAVES EM TODOS OS PAINÉIS
 
-const SUPABASE_URL = 'https://ryvfogmfpjyamgqsltks.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5dmZvZ21mcGp5YW1ncXNsdGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5OTI2NTksImV4cCI6MjA4ODU2ODY1OX0.FSqv0YJwJH5-2Cl4ryhz6o3u7dzfzjD1MOjz2Di1_DA';
+const SUPABASE_URL = 'https://ctobdkstnrhepixyujms.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0b2Jka3N0bnJoZXBpeHl1am1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MTAyOTAsImV4cCI6MjA5Mjk4NjI5MH0.mDuYyQI4rV4KsxN9SsfJj9zmiTx-KZlblcLq22KfIWg';
 
 const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxnystn3UqAKnfLUgw7tXS8CzrnpxTS0iax80rhMCRs-T47ibodGYvbRw2KRrtsnCzn/exec';
 
